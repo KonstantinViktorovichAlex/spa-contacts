9b74da6bc1261eb5a4d3886dd7732b9fa1918960
Konstantin <konstantinwebworks@gmail.com>
2021-01-26 16:28:54 +0300

    table

---
+++ src/pages/Contacts/ContactsTable/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles({
+  table: {},
+});
+
+export const ContactsTable = ({ data }) => {
+  console.log(data);
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="contacts table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Avatar</TableCell>
+            <TableCell>Full name</TableCell>
+            <TableCell>Birthday</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Location</TableCell>
+            <TableCell>Nationality</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((contact) => (
+            <TableRow key={contact.login.uuid}>
+              <TableCell component="th" scope="row">
+                <Avatar alt="Remy Sharp" src={contact.picture.large} />
+              </TableCell>
+              <TableCell>
+                {contact.name.title} {contact.name.first} {contact.name.last}
+              </TableCell>
+              <TableCell>3</TableCell>
+              <TableCell>4</TableCell>
+              <TableCell>5</TableCell>
+              <TableCell>6</TableCell>
+              <TableCell>7</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
